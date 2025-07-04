f421248966eed5f7c76def0efb17524dfc5a40c6
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2025-07-04 17:22:47 +0530

    Views And ViewModel

---
+++ CarRentalHub.API/ViewModels/BookingViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRentalHub.API.ViewModels
+{
+    public class BookingViewModel
+    {
+        public int CarId { get; set; }
+
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public string? Color { get; set; }
+
+        public decimal PricePerDay { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime BookingDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public string FromLocation { get; set; }
+        [Required]
+        public string ToLocation { get; set; }
+
+        public double Distance { get; set; }
+        public decimal CalculatedPrice { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ CarRentalHub.API/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRentalHub.API.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Role { get; set; }
+    }
+}
